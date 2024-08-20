f53fa911f0d625b929419aac5cba80eb30e6ecb9
PrAdEePkUmAr07102 <pradeep07spk@gmail.com>
2024-08-20 15:30:58 +0530

    ROUTE STRUCTURE

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ const app = express();
 const PORT = 7070;
 
 
+//CRUD FUNCTIONALITY OF PRADEEPS FOODS
+
+//R - FOR READING 
+
+app.get("/foods",()=>{});
+
+//C - FOR CREATING 
+
+app.post("/foods",()=>{});
+
+//U -FOR UPDATE
+
+app.put("/foods/1",()=>{});
+
+//D - FOR DELETE
+
+app.delete("/foods/2",()=>{});
+
+
 app.get("/",(req,res)=>{
     res.json({PRADEEPS_KITCHEN : "HELLO BUDDY! WHAT TYPE OF FOOD YOU NEED?"});
 });
